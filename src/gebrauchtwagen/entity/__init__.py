86fb8060299f6254b4625773b920da07aa5686a0
Muhammed Guener <muhammed.guener1453@gmail.com>
2026-04-07 12:17:18 +0200

    Meine Änderungen

---
+++ src/gebrauchtwagen/entity/__init__.py
@@ -0,0 +1,13 @@
+from .base import Base
+from .gebrauchtwagen import Gebrauchtwagen
+from .schaden import Schaden
+from .standort import Standort
+from .hauptuntersuchung import Hauptuntersuchung
+
+__all__ = [
+    "Base",
+    "Gebrauchtwagen",
+    "Schaden",
+    "Standort",
+    "Hauptuntersuchung",
+]
